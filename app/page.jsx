02ae10ab0fef089446dd56fc71fca990b89df0e4
shkTalha33 <shykhtalha33@gmail.com>
2025-09-06 21:09:29 +0500

    unnecessary code removes

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,6 @@
 
 // Import components with dynamic loading for better performance
 import BestSellingProduct from "@/components/home/bestSellingProduct";
-import FeaturedProducts from "@/components/home/featuredProducts";
 import HeroSection from "@/components/home/heroSection";
 import FashionSection from "@/components/home/fashionSection";
 import NewArrivals from "@/components/home/newArrival";
@@ -12,7 +11,6 @@ export default function Home() {
   return (
     <main className="mx-auto">
       <HeroSection />
-      {/* <FeaturedProducts /> */}
       <BestSellingProduct />
       <FeaturedSection />
       <NewArrivals />
--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,23 +1,3 @@
-import house1 from "./house1.jpg";
-import house2 from "./house2.jpg";
-import house3 from "./house3.jpg";
-import house4 from "./house4.jpg";
-import house5 from "./house5.jpg";
-import house6 from "./house6.jpg";
-import house7 from "./house7.jpg";
-import house8 from "./house8.jpg";
-import house9 from "./house9.jpg";
-import house10 from "./house10.jpg";
-import house11 from "./house11.jpg";
-import house12 from "./house12.jpg";
-import house13 from "./house13.jpg";
-import house14 from "./house14.jpg";
-import house15 from "./house15.jpg";
-import team1 from "./team1.jpg";
-import team2 from "./team2.jpg";
-import contact from "./contact.jpg";
-import privacy from "./privacy.jpg";
-import terms from "./terms.jpg";
 import lady1 from "./lady1.jpg";
 import lady2 from "./lady2.jpg";
 
@@ -33,26 +13,6 @@ import dress9 from "./dress9.jpg";
 import dress10 from "./dress10.jpg";
 
 export {
-  house1,
-  house2,
-  house3,
-  house4,
-  house5,
-  house6,
-  house7,
-  house8,
-  house9,
-  house10,
-  house11,
-  house12,
-  house13,
-  house14,
-  house15,
-  team1,
-  team2,
-  contact,
-  privacy,
-  terms,
   lady1,
   lady2,
   dress1,
